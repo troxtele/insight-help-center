977fb7c0a98dd9e440bb0ecb33baa48221d02e0e
troxtele <105340164+troxtele@users.noreply.github.com>
2023-09-18 13:26:54 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const lists = [...document.querySelectorAll(".sidenav a")];
 contents.map((content) => {
   content.querySelector(".label").addEventListener("click", () => {
     const options = content.querySelector(".options ul");
+    content.querySelector(".arrow svg").classList.toggle("rotate");
     options.classList.toggle("heightwidth");
   });
 });
